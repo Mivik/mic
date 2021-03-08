8098e0c8c7e0ba6a57813f650bbf4dbd977e0df4
mivik <mivik@qq.com>
2021-03-08 21:37:07 +0800

    TEMPORARY

--- zen.h
+++ zen.h
@@ -26,14 +26,32 @@ namespace zen {
 
 const auto status_color = mic::term::bg_color(mic::term::green) + mic::term::fg_color(mic::term::white);
 const auto error_color = mic::term::bg_color(mic::term::red) + mic::term::fg_color(mic::term::white);
+const auto subtask_color = mic::term::bg_color(mic::term::green) + mic::term::fg_color(mic::term::black);
 
 inline int map(int x, int lx, int hx, int ly, int hy) { return (int)((double)(x - lx + 1) / (hx - lx + 1) * (hy - ly) + ly); }
 
 inline int cmd(const std::string &str) { return system(str.data()); }
 
+struct Limit { uint32_t time, memory; };
+Limit current_limit = {
+	.time = 1000,
+	.memory = 131072
+};
+
+class Config {
+public:
+	inline size_t size() const { return data.size(); }
+	inline Limit at(uint32_t id) const { return data[id - 1]; }
+private:
+	std::vector<Limit> data;
+
+	friend class Problem;
+};
+
 struct Subtask {
 	std::string name;
-	uint32_t score, num_data;
+	uint32_t id, score, num_data;
+	Limit limit;
 	std::function<void(int, std::ofstream&)> gen;
 };
 
@@ -42,7 +60,7 @@ public:
 	explicit Problem(const std::string &name): name(name) {}
 	inline bool gen();
 	const Subtask& reg(const std::string &name, uint32_t score, uint32_t num_data, std::function<void(int, std::ofstream&)> gen) {
-		tasks.push_back({ name, score, num_data, gen });
+		tasks.push_back({ name, (uint32_t)tasks.size() + 1, score, num_data, current_limit, gen });
 		return tasks.back();
 	}
 private:
@@ -57,32 +75,35 @@ bool Problem::gen() {
 	for (auto &task : tasks) total += task.num_data;
 
 	if (cmd(ZEN_COMPILER " " ZEN_COMPILE_OPTS " " + name + ".cpp -o /tmp/" + name)) {
-		cerr < "Failed to compile\n";
+		cerr < error_color < "Failed to compile" < (reset) < '\n';
 		return false;
 	}
 	std::filesystem::create_directories("data");
+
+	Config config; config.data.reserve(total);
 	uint32_t id = 0;
 	auto info = [&](const Subtask &task, uint32_t cur) -> std::ostream& {
 		reset_line();
-		cout < status_color < '[' < task.name < ']' < (reset) < ' ';
+		cout < status_color < (int)std::round((double)(id + cur - 1) * 100 / total) < '%' < (reset) < ' ';
+		cout < subtask_color < '[' < task.name < ']' < (reset) < ' ';
 		cout < status_color < '[' < cur < '/' < task.num_data < ']' < (reset) < ' ';
-		cout < status_color < (int)std::round((double)(id + cur) * 100 / total) < (reset) < ' ';
 		return cout;
 	};
 	for (auto &task : tasks) {
 		for (uint32_t i = 1; i <= task.num_data; ++i) {
+			config.data.push_back(task.limit);
 			const std::string prefix = "data/" + std::to_string(id + i) + ".";
 			info(task, i) < "Generating input... "; cout.flush();
 			std::ofstream out(prefix + "in"); task.gen(i, out); out.close();
 			info(task, i) < "Generating output... "; cout.flush();
 			if (cmd("/tmp/" + name + " < " + prefix + "in > " + prefix + "out")) {
-				cerr < "\nFailed to execute std.\n";
+				cerr < '\n' < error_color < "Failed to execute std." < (reset) < '\n';
 				return false;
 			}
 		}
 		id += task.num_data;
 	}
-	reset_line(); cout < "Done\n"; cout.flush();
+	reset_line(); cout < status_color < "[100%]" < (reset) < " Done\n"; cout.flush();
 	return true;
 }
 
@@ -91,7 +112,7 @@ inline bool gen(const std::string &name, int amount, const Func &func) {
 	using namespace mic::term;
 
 	if (cmd(ZEN_COMPILER " " ZEN_COMPILE_OPTS " " + name + ".cpp -o /tmp/" + name)) {
-		cerr < "Failed to compile\n";
+		cerr < error_color < "Failed to compile" < (reset) < '\n';
 		return false;
 	}
 	std::filesystem::create_directories("data");
@@ -106,7 +127,7 @@ inline bool gen(const std::string &name, int amount, const Func &func) {
 		std::ofstream out(prefix + "in"); func(id, out); out.close();
 		info() < "Generating output... "; cout.flush();
 		if (cmd("/tmp/" + name + " < " + prefix + "in > " + prefix + "out")) {
-			cerr < "\nFailed to execute std.\n";
+			cerr < '\n' < error_color < "Failed to execute std." < (reset) < '\n';
 			return false;
 		}
 		info() < "Done";
@@ -122,7 +143,7 @@ inline bool check(const std::string &A, const std::string &B, const Func &gen) {
 
 	if (cmd(ZEN_COMPILER " " ZEN_COMPILE_OPTS " " + A + " -o /tmp/A")
 		|| cmd(ZEN_COMPILER " " ZEN_COMPILE_OPTS " " + B + " -o /tmp/B")) {
-		cerr < error_color < "\nFailed to compile\n" < reset;
+		cerr < '\n' < error_color < "Failed to compile" < (reset) < '\n';
 		return false;
 	}
 	int C = 0;
@@ -138,16 +159,16 @@ inline bool check(const std::string &A, const std::string &B, const Func &gen) {
 
 		info() < "Running A... "; cout.flush();
 		if (cmd("/tmp/A < test.in > /tmp/A.out")) {
-			cerr < error_color < "\nFailed to execute A\n" < reset;
+			cerr < '\n' < error_color < "Failed to execute A" < (reset) < '\n';
 			return false;
 		}
 		info() < "Running B... "; cout.flush();
 		if (cmd("/tmp/B < test.in > /tmp/B.out")) {
-			cerr < error_color < "\nFailed to execute B\n" < reset;
+			cerr < '\n' < error_color < "Failed to execute B" < (reset) < '\n';
 			return false;
 		}
 		if (cmd("diff /tmp/A.out /tmp/B.out")) {
-			cerr < error_color < "\nFailed\n" < reset;
+			cerr < '\n' < error_color < "Failed" < (reset) < '\n';
 			cmd("meld /tmp/A.out /tmp/B.out");
 			return false;
 		}
