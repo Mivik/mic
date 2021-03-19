69c9bd1ab53781bc00b7bfae9b480ffabb69c828
mivik <mivik@qq.com>
2021-03-19 19:04:30 +0800

    Fix

--- mint.h
+++ mint.h
@@ -11,12 +11,12 @@ struct mint {
 	mint(int v): v(v) {}
 	explicit mint(qe v): v(v % mod) {}
 	inline mint inv() const { return ksm(v); }
-	inline mint operator+(const mint &t) const { return pro(v + t.v); }
-	inline mint operator-(const mint &t) const { return per(v - t.v); }
+	inline mint operator+(const mint &t) const { return add(v, t.v); }
+	inline mint operator-(const mint &t) const { return sub(v, t.v); }
 	inline mint operator*(const mint &t) const { return (qe)v * t.v % mod; }
 	inline mint operator/(const mint &t) const { return (qe)v * t.inv().v % mod; }
-	inline void operator+=(const mint &t) { v = pro(v + t.v); }
-	inline void operator-=(const mint &t) { v = per(v - t.v); }
+	inline void operator+=(const mint &t) { Add(v, t.v); }
+	inline void operator-=(const mint &t) { Sub(v, t.v); }
 	inline void operator*=(const mint &t) { v = (qe)v * t.v % mod; }
 	inline void operator/=(const mint &t) { v = (qe)v * t.inv().v % mod; }
 	inline bool operator==(const mint &t) const { return v == t.v; }
